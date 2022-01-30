c01c5eb30f53203c6544c2fdc619612d5c33d89d
Natasa Brisudova <98270916+natasabrisudova@users.noreply.github.com>
2022-01-30 20:31:55 +0100

    Update team.cpp

--- Round_A/team.cpp
+++ Round_A/team.cpp
@@ -11,13 +11,14 @@ This contest offers n problems to the participants.
 For each problem we know, which friend is sure about the solution. 
 Help the friends find the number of problems for which they will write a solution.
 
+Input:
 The first input line contains a single integer n (1 ≤ n ≤ 1000) — the number of problems in the contest.
 Then n lines contain three integers each, each integer is either 0 or 1. 
 If the first number in the line equals 1, then Petya is sure about the problem's solution, otherwise he isn't sure.
 The second number shows Vasya's view on the solution, the third number shows Tonya's view. 
 The numbers on the lines are separated by spaces.
-Output
 
+Output:
 Print a single integer — the number of problems the friends will implement on the contest.
 
 Examples
