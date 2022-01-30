ecaad8ea5ec7599a3ed98faed21c95ddaa5ec206
Natasa Brisudova <98270916+natasabrisudova@users.noreply.github.com>
2022-01-30 20:26:05 +0100

    Update team.cpp

--- Round_A/team.cpp
+++ Round_A/team.cpp
@@ -1,4 +1,5 @@
 /*
+
 https://codeforces.com/contest/231/problem/A
 
 One day three best friends Petya, Vasya and Tonya decided to form a team and take part in programming contests. Participants are usually offered several problems during programming contests. Long before the start the friends decided that they will implement a problem if at least two of them are sure about the solution. Otherwise, the friends won't write the problem's solution.
