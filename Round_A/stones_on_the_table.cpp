26276f89cbe86fe7bdd873845d924bab09392011
Natasa Brisudova <98270916+natasabrisudova@users.noreply.github.com>
2022-02-06 20:27:44 +0100

    Create stones_on_the_table.cpp

---
+++ Round_A/stones_on_the_table.cpp
@@ -0,0 +1,42 @@
+/*
+https://codeforces.com/contest/266/problem/A
+
+There are n stones on the table in a row, each of them can be red, green or blue.
+Count the minimum number of stones to take from the table so that any two 
+neighboring stones had different colors. Stones in a row are considered 
+neighboring if there are no other stones between them.
+
+Input
+The first line contains integer n (1 ≤ n ≤ 50) — the number of stones on the table.
+The next line contains string s, which represents the colors of the stones.
+We'll consider the stones in the row numbered from 1 to n from left to right. 
+Then the i-th character s equals "R", if the i-th stone is red, "G", 
+if it's green and "B", if it's blue.
+
+Output
+Print a single integer — the answer to the problem.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int i,n,ans=0;
+    string s;
+    
+    cin >> n >> s;
+
+    for (i=0; i<n; i++){
+        while (s[i]==s[i+1]) {
+            ans++; 
+            i++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+
+
+
